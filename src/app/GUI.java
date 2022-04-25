1c60dece5d079c043c9c23d3300ea8a7e4ad5f5b
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2022-04-25 17:10:40 -0400

    feat(add-card-layout): added card layout

--- src/app/GUI.java
+++ src/app/GUI.java
@@ -1,9 +1,17 @@
 package app;
 
+import java.awt.CardLayout;
+import java.awt.Container;
 import javax.swing.JFrame;
 
 public class GUI {
   private JFrame frame;
+  private Container
+      contentPane; // this is a special variable that will store the content
+                   // pane of the frame so it does not need to be re-read every
+                   // time we want to use it
+  private CardLayout masterLayout; // this CardLayout is being stored because it
+                                   // will be essential in switching views
 
   public GUI() {
     setupGUI();
@@ -12,6 +20,9 @@ public class GUI {
 
   private void setupGUI() {
     frame = new JFrame();
+    contentPane = frame.getContentPane();
+    masterLayout = new CardLayout();
+
     frame.setBounds(100, 100, 450, 300);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
   }
