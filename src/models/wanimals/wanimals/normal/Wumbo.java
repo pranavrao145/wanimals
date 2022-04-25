a3aed6cbdc62d718fc07e6a29e70e320678e33b8
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2022-04-25 12:36:56 -0400

    feat(models): added normal wanimals

---
+++ src/models/wanimals/wanimals/normal/Wumbo.java
@@ -0,0 +1,46 @@
+package models.wanimals.wanimals.normal;
+
+import models.wanimals.types.NormalWanimal;
+
+public class Wumbo extends NormalWanimal {
+  /**
+   * Constructor method: this overload of this method takes an argument for
+   * every possible attribute and uses them to create a new wanimal
+   *
+   * @param name - the name with which to create the new wanimal
+   * @param type - the type with which to create the new wanimal
+   * @param level - the level with which to create the new wanimal
+   * @param maxHitpoints - the max hitpoints with which to create the new
+   *     wanimal
+   * @param currentHitpoints - the current hitpoints with which to create the
+   *     new wanimal
+   * @param baseAttack - the base attack with which to create the new wanimal
+   * @param maxArmor - the max armor with which to create the new wanimal
+   * @param currentArmor - the current armor with which to create the new
+   *     wanimal
+   */
+  public Wumbo(String name, String type, int level, int maxHitpoints,
+               int currentHitpoints, int baseAttack, int maxArmor,
+               int currentArmor) {
+    super(name, type, level, maxHitpoints, currentHitpoints, baseAttack,
+          maxArmor,
+          currentArmor); // call the superconstructor with the given values
+  }
+
+  /**
+   * Constructor method: this overload of this method takes no arguments and
+   * creates a new wanimal with default values with each attribute
+   */
+  public Wumbo() {
+    this.name = "Wumbo";
+    this.type = "normal";
+    this.level = 1;
+    this.maxHitpoints = 150;
+    this.currentHitpoints = 150;
+    this.baseAttack = 10;
+    this.maxArmor = 30;
+    this.currentArmor = 30;
+  }
+
+  public void superPunch() {}
+}
