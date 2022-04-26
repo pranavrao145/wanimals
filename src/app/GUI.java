c5dfefa8c4f20c764db0cb38b32713b72679a7ae
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2022-04-26 17:38:33 -0400

    feat(add-initial-views): added quit functionality

--- src/app/GUI.java
+++ src/app/GUI.java
@@ -3,11 +3,15 @@ package app;
 import java.awt.CardLayout;
 import java.awt.Container;
 import java.awt.Font;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingWorker;
 
 public class GUI {
   // the following set of fields represent the various GUI components used in
@@ -36,6 +40,7 @@ public class GUI {
 
   public GUI() {
     setupGUI();
+    attachListeners();
     frame.setVisible(true);
   }
 
@@ -47,13 +52,13 @@ public class GUI {
     frame.setResizable(false);
     frame.setBounds(100, 100, 450, 300);
     frame.setDefaultCloseOperation(
-        JFrame.EXIT_ON_CLOSE); // overriding default close behaviour so
-                               // custom close methods can run
+        JFrame.DO_NOTHING_ON_CLOSE); // overriding default close behaviour so
+                                     // custom close methods can run
 
     contentPane.setLayout(masterLayout);
 
     panel_title = new JPanel();
-    frame.getContentPane().add(panel_title, "name_12198361239532");
+    frame.getContentPane().add(panel_title, "panel_title");
     panel_title.setLayout(null);
 
     lbl_title = new JLabel("Wanimals");
@@ -75,7 +80,7 @@ public class GUI {
 
     panel_shutdown = new JPanel();
     panel_shutdown.setLayout(null);
-    frame.getContentPane().add(panel_shutdown, "name_13245493571907");
+    frame.getContentPane().add(panel_shutdown, "panel_shutdown");
 
     lbl_shutdownThanks = new JLabel("Thanks for playing!");
     lbl_shutdownThanks.setFont(new Font("Dialog", Font.BOLD, 20));
@@ -85,5 +90,75 @@ public class GUI {
     lbl_shutdownMessage = new JLabel("Shutting down...");
     lbl_shutdownMessage.setBounds(171, 137, 110, 17);
     panel_shutdown.add(lbl_shutdownMessage);
+  }
+
+  /**
+   * This method attaches the appropriate listeners to all components defined
+   * above
+   */
+  private void attachListeners() {
+    /*************************************************************************
+     * GENERAL LISTENERS
+     *************************************************************************/
+
+    // listener to run the quit function when the user closes the window
+    frame.addWindowListener(new WindowAdapter() {
+      @Override
+      public void windowClosing(final WindowEvent e) {
+        quit();
+      }
+    });
+
+    /************************************************************************
+     * TITLE SCREEN LISTENERS
+     *************************************************************************/
+
+    // listener to run the quit function when the user presses the quit button
+    btn_titleQuit.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(final ActionEvent e) {
+        quit();
+      }
+    });
+  }
+
+  /**
+   * This method will be run when the program is going to quit, either by
+   * closing the window or by pressing the quit button
+   */
+  private void quit() {
+    masterLayout.show(contentPane,
+                      "panel_shutdown"); // switch to the shutdown panel
+
+    // using a SwingWorker object, asynchronously wait for 2 seconds and then
+    // quit the program
+    final SwingWorker<Object, Object> worker =
+        new SwingWorker<Object, Object>() {
+          /**
+           * This method sleeps for 1000 milliseconds in a separate thread
+           *
+           * @return - this method returns a generic Object
+           * @throws - this method throws an exception if it fails
+           */
+          @Override
+          protected Object doInBackground() throws Exception {
+            Thread.sleep(2000);
+            return null;
+          }
+
+          /**
+           * This method will automatically fire when the above background
+           * task is done. It will set the label back to the old text.
+           */
+          @Override
+          protected void done() {
+            frame.dispose();
+            System.exit(0);
+            super.done();
+          }
+        };
+
+    worker.execute(); // this method call executes the SwingWorker and the
+                      // declared methods
   }
 }
