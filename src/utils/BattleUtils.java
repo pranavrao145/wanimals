ba9ee781710493c34d60a78f218ff37de7eaa393
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2022-05-12 19:41:50 -0400

    feat(automate-battle): added endBattle method to BattleUtils

--- src/utils/BattleUtils.java
+++ src/utils/BattleUtils.java
@@ -20,7 +20,9 @@ public class BattleUtils {
                    enemy)); // set the current battle to a battle between the
                             // player's first wanimal and the enemy given
 
-    Timer timer = new Timer();
+    Timer timer = new Timer(); // create a new timer object. This will be used
+                               // to check any condition that must be repeatedly
+                               // check during the battle
 
     timer.scheduleAtFixedRate(new TimerTask() {
       @Override
@@ -29,9 +31,17 @@ public class BattleUtils {
               Engine.getCurrentBattle()
                   .isRunning())) { // if there isn't a battle or if it's not
                                    // running
-          timer.cancel();          // cancel the current timer
+          timer.cancel();          // cancel the current timer immediately
         }
       }
     }, new Date(), 2000);
+  }
+
+  /**
+   * This method ends the battle currently in progress
+   */
+  public static void endBattle() {
+    Engine.setCurrentBattle(null); // set the current battle to null (discarding
+                                   // of the info about the current battle
   }
 }
