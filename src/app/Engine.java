c62bad14fe88b647634cbc056270ee765f2bea57
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2022-05-20 21:15:26 -0400

    style(write): styled write function

--- src/app/Engine.java
+++ src/app/Engine.java
@@ -5,6 +5,7 @@ import java.io.FileWriter;
 import java.io.PrintWriter;
 import models.battles.battles.Battle;
 import models.player.Player;
+import models.wanimals.Wanimal;
 
 public class Engine {
   private static GUI gui;
@@ -28,66 +29,78 @@ public class Engine {
   public static void setCurrentBattle(Battle currentBattle) {
     Engine.currentBattle = currentBattle;
   }
-  
-  public static void saveWriteToFile () {
-	  
-	  try {
-		  
-		  //Creating the file to write to
-		  final File saveDataFile = new File("saveData.txt");
-		  
-		  //Deleting old file as it is being overwritten
-		  saveDataFile.delete();
-		  
-		  //Select parent file
-		  saveDataFile.getParentFile().mkdirs();
-		  
-		  //Creating the new file
-	      saveDataFile.createNewFile();
-	      
-	      //Creating the file writer
-	      final PrintWriter saveDataWriter = new PrintWriter(new FileWriter(saveDataFile, true));
-	      
-	      //Creating a string that displays all the information on the player in following format
-	      	//Name, realm number, potion number, armor plate number, level, current XP, max XP
-	      String studentContent = String.format("%s;%d;%d;%d;%d;%d;%d", Engine.getPlayer().getName(), 
-	    		  Engine.getPlayer().getRealm(), Engine.getPlayer().getNumPotions(), Engine.getPlayer().getNumArmorPlates(), 
-	    		  Engine.getPlayer().getLevel(), Engine.getPlayer().getCurrentXP(), Engine.getPlayer().getMaxXP());
-	      
-	      //Writing string to file
-	      saveDataWriter.println(studentContent);
-	      
-	      //Finding how many wanimals the player has
-	      int length = Engine.getPlayer().getWanimals().size();
-	      
-	      //Writing the amount to the file
-	      saveDataWriter.println(String.valueOf(length));
-	      
-	      //For loop that runs as many times are there are wanimals in player inventory
-	      for (int x = 0; x < length; x++) {
-	    	  
-	    	  //String of all info of the wanimal in following format
-	    	  	//Name, type, level, current XP, max XP, current HP, max HP, current armor, max armor, first attack name, first attack type,
-	    	  	//second attack name, second attack type
-	    	  String wanimalContent = String.format("%s;%d;%d;%d;%d;%d;%d;%d;%d;%s;%d;%s;%d", Engine.getPlayer().getWanimals().get(x).getName(),
-	    			  Engine.getPlayer().getWanimals().get(x).getType(), Engine.getPlayer().getWanimals().get(x).getLevel(),
-	    			  Engine.getPlayer().getWanimals().get(x).getCurrentXP(), Engine.getPlayer().getWanimals().get(x).getMaxXP(),
-	    			  Engine.getPlayer().getWanimals().get(x).getCurrentHitpoints(), Engine.getPlayer().getWanimals().get(x).getMaxHitpoints(),
-	    			  Engine.getPlayer().getWanimals().get(x).getCurrentArmor(), Engine.getPlayer().getWanimals().get(x).getMaxArmor(),
-	    			  Engine.getPlayer().getWanimals().get(x).getFirstAttack().getName(), Engine.getPlayer().getWanimals().get(x).getFirstAttack().getType(),
-	    			  Engine.getPlayer().getWanimals().get(x).getSecondAttack().getName(), Engine.getPlayer().getWanimals().get(x).getSecondAttack().getType());
-	    	  
-	    	  //Writing this wanimals info to file
-	    	  saveDataWriter.println(wanimalContent);
-	    	  
-	      }
-	      
-	      //Closing the writer
-	      saveDataWriter.close();
-	      
-	  }
-	  
-	  //Catch for any exceptions
-	  catch ( final Exception e) { e.printStackTrace(); }
+
+  /**
+   * This method saves all the information about the current game into a text
+   * file so it may be loaded when necessary. Note: this method will overwrite
+   * any previously saved data
+   */
+  public static void saveWriteToFile() {
+    try {
+      // Creating the file to write to
+      final File saveDataFile = new File("data/saveData.txt");
+
+      // Deleting old file as it is being overwritten
+      saveDataFile.delete();
+
+      // Select parent file
+      saveDataFile.getParentFile().mkdirs();
+
+      // Creating the new file
+      saveDataFile.createNewFile();
+
+      // Creating the file writer
+      final PrintWriter saveDataWriter =
+          new PrintWriter(new FileWriter(saveDataFile, true));
+
+      // Creating a string that displays all the information on the player in
+      // following format Name, realm number, potion number, armor plate number,
+      // level, current XP, max XP
+      String studentContent = String.format(
+          "%s;%d;%d;%d;%d;%d;%d", Engine.getPlayer().getName(),
+          Engine.getPlayer().getRealm(), Engine.getPlayer().getNumPotions(),
+          Engine.getPlayer().getNumArmorPlates(), Engine.getPlayer().getLevel(),
+          Engine.getPlayer().getCurrentXP(), Engine.getPlayer().getMaxXP());
+
+      // Writing string to file
+      saveDataWriter.println(studentContent);
+
+      // Finding how many wanimals the player has
+      int length = Engine.getPlayer().getWanimals().size();
+
+      // Writing the amount to the file
+      saveDataWriter.println(String.valueOf(length));
+
+      // For loop that runs as many times are there are wanimals in player
+      // inventory
+      for (int x = 0; x < length; x++) {
+        Wanimal currentWanimal = Engine.getPlayer().getWanimals().get(x);
+
+        // get the current wanimal
+        // String of all info of the wanimal in following format
+        // Name, type, level, current XP, max XP, current HP, max HP,
+        // current armor, max armor, first attack name, first attack
+        // type, second attack name, second attack type
+        String wanimalContent = String.format(
+            "%s;%d;%d;%d;%d;%d;%d;%d;%d;%s;%d;%s;%d", currentWanimal.getName(),
+            currentWanimal.getType(), currentWanimal.getLevel(),
+            currentWanimal.getCurrentXP(), currentWanimal.getMaxXP(),
+            currentWanimal.getCurrentHitpoints(),
+            currentWanimal.getMaxHitpoints(), currentWanimal.getCurrentArmor(),
+            currentWanimal.getMaxArmor(),
+            currentWanimal.getFirstAttack().getName(),
+            currentWanimal.getFirstAttack().getType(),
+            currentWanimal.getSecondAttack().getName(),
+            currentWanimal.getSecondAttack().getType());
+
+        // Writing this wanimals info to file
+        saveDataWriter.println(wanimalContent);
+      }
+
+      // Closing the writer
+      saveDataWriter.close();
+    } catch (final Exception e) { // Catch for any exceptions
+      e.printStackTrace();        // print the error
+    }
   }
 }
